89234fcd30738a8e2ff900e3ecdd2f93eff3d438
Jelani Canty <jelani.canty@gmail.com>
2023-04-22 21:54:19 -0500

    zoom into schedule

--- ValentineClub/Controller/ScheduleDetailsViewController.swift
+++ ValentineClub/Controller/ScheduleDetailsViewController.swift
@@ -9,15 +9,25 @@ import UIKit
 import Parse
 import AlamofireImage
 
-class ScheduleDetailsViewController: UIViewController {
+class ScheduleDetailsViewController: UIViewController, UIScrollViewDelegate {
     
-
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var scheduleImage: UIImageView!
+    
     var scheduleImageUrl: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.delegate = self
+        scrollView.maximumZoomScale = 3.0
+        scrollView.minimumZoomScale = 1.0
+        
         scheduleImage.af.setImage(withURL: URL(string: scheduleImageUrl)!)
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.scheduleImage
     }
 
 }
