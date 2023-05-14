beffd3137cbf467206075eaac0966280bdd0a9e9
Jelani Canty <jelani.canty@gmail.com>
2023-05-14 11:49:24 -0500

    Logout button

--- ValentineClub/Controller/HomeViewController.swift
+++ ValentineClub/Controller/HomeViewController.swift
@@ -17,27 +17,27 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(HomeViewController.imageTapped(gesture:)))
-
-                // add it to the image view;
-                frontImage.addGestureRecognizer(tapGesture)
-                // make sure imageView can be interacted with by user
-                frontImage.isUserInteractionEnabled = true
+//        // Do any additional setup after loading the view.
+//
+//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(HomeViewController.imageTapped(gesture:)))
+//
+//                // add it to the image view;
+//                frontImage.addGestureRecognizer(tapGesture)
+//                // make sure imageView can be interacted with by user
+//                frontImage.isUserInteractionEnabled = true
     }
     
     
-    @objc func imageTapped(gesture: UIGestureRecognizer) {
-            // if the tapped view is a UIImageView then set it to imageview
-            if (gesture.view as? UIImageView) != nil {
-                if (tapCounter < 2) {
-                    tapCounter = tapCounter + 1
-                } else {
-                    PFUser.logOut()
-                    dismiss(animated: true, completion: nil)
-                }
-            }
-        }
+//    @objc func imageTapped(gesture: UIGestureRecognizer) {
+//            // if the tapped view is a UIImageView then set it to imageview
+//            if (gesture.view as? UIImageView) != nil {
+//                if (tapCounter < 2) {
+//                    tapCounter = tapCounter + 1
+//                } else {
+//                    PFUser.logOut()
+//                    dismiss(animated: true, completion: nil)
+//                }
+//            }
+//        }
 
 }
